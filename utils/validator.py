4f0619a0b0c1a6cab5ed4ee165a41e1fcd3bba0f
dansolbe <daniil.k.solbe@mail.ru>
2025-12-01 20:18:04 +0500

    minor fix

--- utils/validator.py
+++ utils/validator.py
@@ -1,10 +1,12 @@
 from typing import Any, Type, TypeVar
 
+import allure
 from pydantic import BaseModel
 
 T = TypeVar("T", bound=BaseModel)
 
 
+@allure.step("Валидация/сериализация данных ответа")
 def response_validation(data: dict[str, Any], model: Type[T]) -> T:
     try:
         v_data = model.model_validate(data)
